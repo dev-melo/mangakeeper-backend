ffdd97ec4669a6b8366231a14348b8819c784ca3
Dev Melo <omelodev@gmail.com>
2026-04-16 12:03:37 -0300

    feat: controller: getUsuarioById() ok

--- src/main/java/dev/flmelo/mangakeeper/backend/controller/UsuarioController.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import dev.flmelo.mangakeeper.backend.dto.UsuarioResponseDTO;
 import dev.flmelo.mangakeeper.backend.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class UsuarioController {
     public ResponseEntity<List<UsuarioResponseDTO>> getAll(){
         List<UsuarioResponseDTO> list = usuarioService.getAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsuarioResponseDTO> getUsuarioById(@PathVariable Long id){
+        UsuarioResponseDTO usuario = usuarioService.getById(id);
+        return ResponseEntity.ok().body(usuario);
     }
 }
--- src/main/java/dev/flmelo/mangakeeper/backend/service/UsuarioService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/service/UsuarioService.java
@@ -1,10 +1,13 @@
 package dev.flmelo.mangakeeper.backend.service;
 
 import dev.flmelo.mangakeeper.backend.dto.UsuarioResponseDTO;
+import dev.flmelo.mangakeeper.backend.entity.Usuario;
 import dev.flmelo.mangakeeper.backend.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -23,6 +26,18 @@ public class UsuarioService {
                         usuario.getUsername(),
                         usuario.getAvatarUrl()
                 )).toList();
+
+    }
+
+    public UsuarioResponseDTO getById(Long id) {
+        Optional<Usuario> byId = usuarioRepository.findById(id);
+        if (byId.isEmpty())
+        {
+            throw new EntityNotFoundException("Usuario não encontrado: ID " + id);
+        }
+
+        Usuario u = byId.get();
+        return new UsuarioResponseDTO(u.getId(), u.getUsername(), u.getAvatarUrl());
 
     }
 }
