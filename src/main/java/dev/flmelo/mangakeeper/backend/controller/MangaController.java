8e7f135aff6a41f78da6bee7ba2ca2eb7b3c508a
Dev Melo <omelodev@gmail.com>
2026-04-21 16:58:20 -0300

    feat: getAll Mangas

--- src/main/java/dev/flmelo/mangakeeper/backend/controller/MangaController.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/MangaController.java
@@ -1,8 +1,13 @@
 package dev.flmelo.mangakeeper.backend.controller;
 
+import dev.flmelo.mangakeeper.backend.dto.manga.MangaResponseDTO;
 import dev.flmelo.mangakeeper.backend.service.MangaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "mangas")
@@ -13,4 +18,12 @@ public class MangaController {
     public MangaController(MangaService mangaService) {
         this.mangaService = mangaService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<MangaResponseDTO>> getAll(){
+        List<MangaResponseDTO> list = mangaService.getAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+
 }
--- src/main/java/dev/flmelo/mangakeeper/backend/service/MangaService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/service/MangaService.java
@@ -1,7 +1,10 @@
 package dev.flmelo.mangakeeper.backend.service;
 
+import dev.flmelo.mangakeeper.backend.dto.manga.MangaResponseDTO;
 import dev.flmelo.mangakeeper.backend.repository.MangaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MangaService {
@@ -10,5 +13,22 @@ public class MangaService {
 
     public MangaService(MangaRepository mangaRepository) {
         this.mangaRepository = mangaRepository;
+    }
+
+    public List<MangaResponseDTO> getAll(){
+        return mangaRepository.findAll()
+                .stream()
+                .map(manga ->  new MangaResponseDTO(
+                        manga.getId(),
+                        manga.getTitulo(),
+                        manga.getAutor(),
+                        manga.getEditora(),
+                        manga.getGenero(),
+                        manga.getSinopse(),
+                        manga.getIssn(),
+                        manga.getTotalVolumes(),
+                        manga.getIdioma(),
+                        manga.getColecao().getId()
+                )).toList();
     }
 }
