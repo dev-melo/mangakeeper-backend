08d935cb107fa877f71637d09eb7c0537d6b6123
Dev Melo <omelodev@gmail.com>
2026-04-20 21:20:29 -0300

    feat: add endpoint colecoes/id

--- src/main/java/dev/flmelo/mangakeeper/backend/controller/ColecaoController.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/ColecaoController.java
@@ -4,6 +4,7 @@ import dev.flmelo.mangakeeper.backend.dto.colecao.ColecaoResponseDTO;
 import dev.flmelo.mangakeeper.backend.service.ColecaoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class ColecaoController {
     public ResponseEntity<List<ColecaoResponseDTO>> getAll(){
         List<ColecaoResponseDTO> list = colecaoService.getAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ColecaoResponseDTO> getById(@PathVariable Long id){
+        ColecaoResponseDTO colecao = colecaoService.getById(id);
+        return ResponseEntity.ok().body(colecao);
     }
 }
