ec19ea503f985ed37f8545a3ad17606a1c20b425
Dev Melo <omelodev@gmail.com>
2026-04-20 21:14:55 -0300

    feat: add service getColecao byId

--- src/main/java/dev/flmelo/mangakeeper/backend/service/ColecaoService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/service/ColecaoService.java
@@ -1,10 +1,14 @@
 package dev.flmelo.mangakeeper.backend.service;
 
 import dev.flmelo.mangakeeper.backend.dto.colecao.ColecaoResponseDTO;
+import dev.flmelo.mangakeeper.backend.entity.Colecao;
 import dev.flmelo.mangakeeper.backend.repository.ColecaoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ColecaoService {
@@ -24,5 +28,21 @@ public class ColecaoService {
                         colecao.getPublico(),
                         colecao.getUsuario().getId()
                 )).toList();
+    }
+
+    public ColecaoResponseDTO getById(Long id){
+        Optional<Colecao> colecaoById = colecaoRepository.findById(id);
+        if (colecaoById.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Coleção não encontrada.");
+        }
+
+        Colecao colecao = colecaoById.get();
+
+        return new ColecaoResponseDTO(
+                colecao.getId(),
+                colecao.getNome(),
+                colecao.getPublico(),
+                colecao.getUsuario().getId());
+
     }
 }
