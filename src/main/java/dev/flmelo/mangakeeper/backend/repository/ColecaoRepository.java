075d6af202f49979f8d3d9a59defca7d5f38a947
Dev Melo <omelodev@gmail.com>
2026-04-24 20:14:35 -0300

    feat: cria VolumeRepository, add @Repository nas demais interfaces repositories

--- src/main/java/dev/flmelo/mangakeeper/backend/repository/ColecaoRepository.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/repository/ColecaoRepository.java
@@ -2,6 +2,8 @@ package dev.flmelo.mangakeeper.backend.repository;
 
 import dev.flmelo.mangakeeper.backend.entity.Colecao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ColecaoRepository extends JpaRepository<Colecao, Long> {
 }
--- src/main/java/dev/flmelo/mangakeeper/backend/repository/VolumeRepository.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/repository/VolumeRepository.java
@@ -1,9 +1,9 @@
 package dev.flmelo.mangakeeper.backend.repository;
 
-import dev.flmelo.mangakeeper.backend.entity.Manga;
+import dev.flmelo.mangakeeper.backend.entity.Volume;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MangaRepository extends JpaRepository<Manga, Long> {
+public interface VolumeRepository extends JpaRepository<Volume, Long> {
 }
