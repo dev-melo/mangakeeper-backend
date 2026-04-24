9c5d7f54d2b827f79e6eb18646ccedeb02237ce3
Dev Melo <omelodev@gmail.com>
2026-04-23 23:30:49 -0300

    feat: adiciona handler global para exceções de domínio (UserNotFound, UserAlreadyExists)

---
+++ src/main/java/dev/flmelo/mangakeeper/backend/infra/RestExceptionHandler.java
@@ -0,0 +1,24 @@
+package dev.flmelo.mangakeeper.backend.infra;
+
+import dev.flmelo.mangakeeper.backend.exceptions.UserAlreadyExistsException;
+import dev.flmelo.mangakeeper.backend.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    private ResponseEntity<String> userNotFoundHandler(UserNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    private ResponseEntity<String> userAlreadyExistsHandler(UserAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+
+}
