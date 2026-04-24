66cfc15ae87024abe3f3b18039c64424e3e09543
Dev Melo <omelodev@gmail.com>
2026-04-24 20:50:26 -0300

    feat: cria getAll no VolumeController

---
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/VolumeController.java
@@ -0,0 +1,27 @@
+package dev.flmelo.mangakeeper.backend.controller;
+
+import dev.flmelo.mangakeeper.backend.dto.volume.VolumeResponseDTO;
+import dev.flmelo.mangakeeper.backend.service.VolumeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "volumes")
+public class VolumeController {
+
+    public VolumeController(VolumeService volumeService) {
+        this.volumeService = volumeService;
+    }
+
+    private final VolumeService volumeService;
+
+    @GetMapping
+    public ResponseEntity<List<VolumeResponseDTO>> getAll(){
+        List<VolumeResponseDTO> list = volumeService.getAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
