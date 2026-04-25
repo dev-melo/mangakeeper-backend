58c50aaab3772f47a5d4664adcf05a6479e6c990
Dev Melo <omelodev@gmail.com>
2026-04-24 21:06:49 -0300

    feat: cria getById em VolumeService

--- src/main/java/dev/flmelo/mangakeeper/backend/service/VolumeService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/service/VolumeService.java
@@ -1,12 +1,16 @@
 package dev.flmelo.mangakeeper.backend.service;
 
 import dev.flmelo.mangakeeper.backend.dto.volume.VolumeResponseDTO;
+import dev.flmelo.mangakeeper.backend.entity.Volume;
 import dev.flmelo.mangakeeper.backend.repository.MangaRepository;
 import dev.flmelo.mangakeeper.backend.repository.VolumeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VolumeService {
@@ -31,4 +35,19 @@ public class VolumeService {
                 )).toList();
     }
 
+    public VolumeResponseDTO getById(Long id){
+        Optional<Volume> volumeById = volumeRepository.findById(id);
+        if (volumeById.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Volume não encontrado.");
+        }
+        Volume v = volumeById.get();
+        return new VolumeResponseDTO(
+                v.getId(),
+                v.getNumero(),
+                v.getCodigoDeBarras(),
+                v.getIsbn(),
+                v.getManga().getId(),
+                v.getImagemUrl()
+        );
+    }
 }
