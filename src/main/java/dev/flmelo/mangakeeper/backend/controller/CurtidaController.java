05a8fc14c2987092edf3a7a3b83a7ae319d6cfc0
Dev Melo <omelodev@gmail.com>
2026-04-26 21:09:09 -0300

    feat: cria endpoint e metodo para curtir colecao

--- src/main/java/dev/flmelo/mangakeeper/backend/controller/CurtidaController.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/CurtidaController.java
@@ -1,7 +1,24 @@
 package dev.flmelo.mangakeeper.backend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dev.flmelo.mangakeeper.backend.service.CurtidaService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "colecoes")
 public class CurtidaController {
+
+    private final CurtidaService curtidaService;
+
+    public CurtidaController(CurtidaService curtidaService) {
+        this.curtidaService = curtidaService;
+    }
+
+    @PostMapping("/{colecaoId}/likes")
+    public ResponseEntity<?> curtirColecao(@Valid @PathVariable Long colecaoId, @RequestParam Long usuarioId){
+        curtidaService.curtirColecao(usuarioId, colecaoId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
