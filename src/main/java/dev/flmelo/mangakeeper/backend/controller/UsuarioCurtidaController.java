7d6b6361e6d13ef9b3c789c328f510b59192a1c1
Dev Melo <omelodev@gmail.com>
2026-04-28 15:14:57 -0300

    feat: documentação para UsuarioCurtidaController

--- src/main/java/dev/flmelo/mangakeeper/backend/controller/UsuarioCurtidaController.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/UsuarioCurtidaController.java
@@ -2,6 +2,10 @@ package dev.flmelo.mangakeeper.backend.controller;
 
 import dev.flmelo.mangakeeper.backend.dto.curtida.UsuarioCurtidaResponseDTO;
 import dev.flmelo.mangakeeper.backend.service.CurtidaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("usuarios")
+@Tag(name = "Usuário - Curtidas", description = "Operações relacionadas às curtidas de usuários")
 public class UsuarioCurtidaController {
 
     private final CurtidaService curtidaService;
@@ -17,7 +22,11 @@ public class UsuarioCurtidaController {
     public UsuarioCurtidaController(CurtidaService curtidaService) {
         this.curtidaService = curtidaService;
     }
-
+    @Operation(summary = "Listar curtidas do usuário", description = "Retorna o total de curtidas e as coleções curtidas por um usuário específico")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna resumo com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @GetMapping("/{usuarioId}/likes")
     public ResponseEntity<UsuarioCurtidaResponseDTO> curtidasUsuario(@PathVariable Long usuarioId){
         UsuarioCurtidaResponseDTO curtidaDTO = curtidaService.totalCurtidasUsuario(usuarioId);
