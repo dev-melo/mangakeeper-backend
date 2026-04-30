16327814a546341570681cc0b5c17ab0a6ebc021
Dev Melo <omelodev@gmail.com>
2026-04-30 15:58:11 -0300

    feat: disable csrf poderia usar tbm .csrf(AbstractHttpConfigurer::disable)

--- src/main/java/dev/flmelo/mangakeeper/backend/config/security/WebSecurityConfig.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/config/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().permitAll()
                 )
