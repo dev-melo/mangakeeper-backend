e134a42aca5f27a77095b24abf3ee2bc8218f4b2
Dev Melo <omelodev@gmail.com>
2026-05-01 01:42:16 -0300

    feat: cria entidade para controle de funções dos usuarios

---
+++ src/main/java/dev/flmelo/mangakeeper/backend/entity/RoleModel.java
@@ -0,0 +1,26 @@
+package dev.flmelo.mangakeeper.backend.entity;
+
+import dev.flmelo.mangakeeper.backend.entity.enuns.RoleName;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@Setter
+public class RoleModel implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+
+
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+}
