4435563f8a8562e3609a8e622bc699fe4290ace9
Dev Melo <omelodev@gmail.com>
2026-05-01 02:28:21 -0300

    feat: add @Transactional

--- src/main/java/dev/flmelo/mangakeeper/backend/config/security/UsuarioDetailsService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/config/security/UsuarioDetailsService.java
@@ -8,10 +8,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UsuarioDetailsService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
