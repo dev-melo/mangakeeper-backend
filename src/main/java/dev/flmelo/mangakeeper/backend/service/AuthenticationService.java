ecb53196dc8b7b63922a7696bfdfb1439148bc05
Dev Melo <omelodev@gmail.com>
2026-05-06 15:53:53 -0300

    feat: add logica para a criação do usuario via /auth/registar

--- src/main/java/dev/flmelo/mangakeeper/backend/service/AuthenticationService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package dev.flmelo.mangakeeper.backend.service;
 
 import dev.flmelo.mangakeeper.backend.dto.security.RegisterDTO;
+import dev.flmelo.mangakeeper.backend.entity.RoleModel;
 import dev.flmelo.mangakeeper.backend.entity.Usuario;
+import dev.flmelo.mangakeeper.backend.entity.enuns.RoleName;
+import dev.flmelo.mangakeeper.backend.repository.RoleRepository;
 import dev.flmelo.mangakeeper.backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -9,15 +12,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuthenticationService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+
     public ResponseEntity register(RegisterDTO data){
         if (usuarioRepository.findByUsername(data.login()).isPresent()) return ResponseEntity.badRequest().build();
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        Usuario newUser = new Usuario(data.login(), encryptedPassword, data.role());
+        Usuario newUser = new Usuario(data.login(), data.email(), encryptedPassword,  data.avatarUrl());
+        RoleModel role = roleRepository.findByRoleName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Role não Encontrada"));
+        newUser.setRoles(List.of(role));
         try{
             usuarioRepository.save(newUser);
         } catch (DataIntegrityViolationException e){
