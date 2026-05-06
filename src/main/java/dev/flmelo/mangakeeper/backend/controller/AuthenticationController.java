2bd97554293309ce2389d93736652c5d2344bd04
Dev Melo <omelodev@gmail.com>
2026-05-06 01:06:26 -0300

    feat: cria AuthenticationController

---
+++ src/main/java/dev/flmelo/mangakeeper/backend/controller/AuthenticationController.java
@@ -0,0 +1,11 @@
+package dev.flmelo.mangakeeper.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "auth")
+public class AuthenticationController {
+
+
+}
