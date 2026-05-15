464689e20f4a3f0a18afff28b93c83aec6718473
Dev Melo <omelodev@gmail.com>
2026-05-15 02:09:37 -0300

    feat: cocria o metodo uploadImage

--- src/main/java/dev/flmelo/mangakeeper/backend/service/CloudinaryService.java
+++ src/main/java/dev/flmelo/mangakeeper/backend/service/CloudinaryService.java
@@ -1,4 +1,28 @@
 package dev.flmelo.mangakeeper.backend.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file){
+        try{
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.emptyMap()
+            );
+            return uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao fazer upload da imagem");
+        }
+    }
 }
